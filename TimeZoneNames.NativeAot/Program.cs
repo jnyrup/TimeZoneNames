9d55806aa0a7a5ba3ec5748a1fa6906d3c364158
Jonas Nyrup <jnyrup@gmail.com>
2022-10-01 17:26:59 +0200

    NativeAOT test application

---
+++ TimeZoneNames.NativeAot/Program.cs
@@ -0,0 +1,10 @@
+﻿using TimeZoneNames;
+
+Console.WriteLine("Hello world");
+IDictionary<string, string> displayNames = TZNames.GetDisplayNames("fr-CA");
+string pacific = displayNames["Pacific Standard Time"];
+Console.WriteLine(pacific);
+
+// Exercise some code calling TZConvert
+string displayName = TZNames.GetDisplayNameForTimeZone("Yukon Standard Time", "en");
+Console.WriteLine(displayName);
